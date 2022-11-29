3572a70c3deb5b6b822fa76816ff9d47cad9c992
Mitch Rucker <mitchruckerks@gmail.com>
2022-11-29 14:26:27 -0600

    linked js to html. created function to build the deckk of cards

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
 // Global variables
-const suit = ['spades', 'hearts', 'diamonds', 'clubs']
-const rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-const hand = ['High Card', 'Pair', 'Two Pair', 'Three of a Kind', 'Straight', 'Flush', 'Full House', 'Four of a Kind', 'Straight Flush']
+const suits = ['spades', 'hearts', 'diamonds', 'clubs']
+const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+const cards = []
+const hands = ['High Card', 'Pair', 'Two Pair', 'Three of a Kind', 'Straight', 'Flush', 'Full House', 'Four of a Kind', 'Straight Flush']
 
 // Classes
-class Card {
-    constructor(suit, rank){
-        this.suit = suit;
-        this.rank = rank;
-    }
-}
 
-class Deck {
-    constructor(suit, rank){
-        this.suit = suit;
-        this.rank = rank;
-    }
-    shuffle(){
-        //USE GOOGLE
-    }
-    dealStart(){
-        //Deal 2 hole cards to each player
-    }
-    dealFlop(){
-        //Deal 3 community cards
-    }
-    dealTurn(){
-        //Deal 1 community card
-    }
-    dealRiver(){
-        //Deal 1 community card
+// class Deck {
+//     constructor(suit, rank){
+//         this.suit = suit;
+//         this.rank = rank;
+//     }
+   
+//     shuffle(){
+//         // Randomize order of objects in array
+//     }
+//     dealStart(){
+//         //Deal 2 hole cards to each player
+//     }
+//     dealFlop(){
+//         //Deal 3 community cards
+//     }
+//     dealTurn(){
+//         //Deal 1 community card
+//     }
+//     dealRiver(){
+//         //Deal 1 community card
+//     }
+// }
+function buildDeck(){
+    for (let i=0; i<suits.length; i++){
+        for (let x=0; x<ranks.length; x++){
+            let newCard = {Rank: ranks[x], Suit: suits[i]}
+            cards.push(newCard);
+        }
     }
 }
+buildDeck();
+console.log(cards);
