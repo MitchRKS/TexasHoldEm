6897330fe67577f9c75f365e7dd88e55f0be627c
Mitch Rucker <mitchruckerks@gmail.com>
2022-11-29 21:34:59 -0600

    deck now built within the Class

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
-// Global variables
-const suits = ['spades', 'hearts', 'diamonds', 'clubs']
-const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-const cards = []
-const hands = ['High Card', 'Pair', 'Two Pair', 'Three of a Kind', 'Straight', 'Flush', 'Full House', 'Four of a Kind', 'Straight Flush']
+// // Global variables
 
-// Classes
+// const hands = ['High Card', 'Pair', 'Two Pair', 'Three of a Kind', 'Straight', 'Flush', 'Full House', 'Four of a Kind', 'Straight Flush']
 
-// class Deck {
-//     constructor(suit, rank){
-//         this.suit = suit;
-//         this.rank = rank;
-//     }
-   
-//     shuffle(){
-//         // Randomize order of objects in array
-//     }
-//     dealStart(){
-//         //Deal 2 hole cards to each player
-//     }
-//     dealFlop(){
-//         //Deal 3 community cards
-//     }
-//     dealTurn(){
-//         //Deal 1 community card
-//     }
-//     dealRiver(){
-//         //Deal 1 community card
-//     }
-// }
-function buildDeck(){
-    for (let i=0; i<suits.length; i++){
-        for (let x=0; x<ranks.length; x++){
-            let newCard = {Rank: ranks[x], Suit: suits[i]}
-            cards.push(newCard);
+// //Classes
+
+class Deck {
+    constructor(){
+        this.deck = []
+        const suits = ['spades', 'hearts', 'diamonds', 'clubs']
+        const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        for (let suit in suits){
+            for (let rank in ranks){
+                this.deck.push(`${ranks[rank]} of ${suits[suit]}`);
+            }
         }
     }
 }
-buildDeck();
-console.log(cards);
+const deckOne = new Deck;
+console.log(deckOne.deck);
+
+// class Deck {
+//     constructor() {
+//       this.deck = [];
+//       const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+//       const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+  
+//       for (let suit in suits) {
+//         for (let value in values) {
+//           this.deck.push(`${values[value]} of ${suits[suit]}`);
+//         }
+//       }
+//     }
+//   }
+  
+//   const deck1 = new Deck();
+//   console.log(deck1.deck);
