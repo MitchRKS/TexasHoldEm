7d0a1bf128d7b75cd8bdaa6bb9edb7ca5ce26048
Mitch Rucker <mitchruckerks@gmail.com>
2022-12-01 15:31:08 -0600

    buttons added, can call methods on the deck

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const bodyEl = document.querySelector('body');
 const playerDiv = document.createElement("div")
 playerDiv.classList.add('character-div')
 bodyEl.appendChild(playerDiv);
+
 const computerDiv = document.createElement("div")
 computerDiv.classList.add('character-div')
 bodyEl.appendChild(computerDiv);
+
 const boardDiv = document.createElement("div")
+boardDiv.textContent = 'Community Cards'
 bodyEl.appendChild(boardDiv);
 
 const playerHoleCards = document.createElement("div")
@@ -20,12 +23,15 @@ const flop = document.createElement("div");
 const turn = document.createElement("div");
 
 const river = document.createElement("div");
+
+
+
 // Classes
 class Deck {
     constructor(){
         this.deck = []
         const suits = ['spades', 'hearts', 'diamonds', 'clubs']
-        const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
         for (let suit in suits){
             for (let rank in ranks){
                 const card = {
@@ -50,11 +56,15 @@ class Deck {
         const holeCardOne = playerCards[0];
         const holeCardTwo = playerCards[1];
         playerHoleCards.textContent = `${holeCardOne.rank} of ${holeCardOne.suit} | ${holeCardTwo.rank} of ${holeCardTwo.suit}`
+        playerHoleCards.classList.add("card")
+        playerHoleCards.setAttribute("id", "player-hole-cards");
         playerDiv.appendChild(playerHoleCards);
         const compCards = this.deck.splice(0, 2);
         const holeCardThree = compCards[0];
         const holeCardFour = compCards[1];
         compHoleCards.textContent = `${holeCardThree.rank} of ${holeCardThree.suit} | ${holeCardFour.rank} of ${holeCardFour.suit}`
+        compHoleCards.classList.add("card")
+        compHoleCards.setAttribute("id", "comp-hole-cards");
         computerDiv.appendChild(compHoleCards);
     }
     dealFlop(){
@@ -66,38 +76,57 @@ class Deck {
         const flopCardThree = flopCards[2];
         console.log(flopCardThree);
         flop.textContent = `${flopCardOne.rank} of ${flopCardOne.suit} | ${flopCardTwo.rank} of ${flopCardTwo.suit} | ${flopCardThree.rank} of ${flopCardThree.suit}`
+        flop.classList.add("card")
         bodyEl.appendChild(flop);
     }
     dealTurn(){
         const turnCard = this.deck.splice(0, 1)[0];
         turn.textContent = `${turnCard.rank} of ${turnCard.suit}`
+        turn.classList.add("card");
         bodyEl.appendChild(turn);
     }
     dealRiver(){
         const riverCard = this.deck.splice(0, 1)[0];
         river.textContent = `${riverCard.rank} of ${riverCard.suit}`
+        river.classList.add("card");
         bodyEl.appendChild(river);
     }
+    
 }
 const deckOne = new Deck
-deckOne.shuffle();
-deckOne.dealStart();
-deckOne.dealFlop();
-deckOne.dealTurn();
-deckOne.dealRiver();
+// deckOne.shuffle();
+// deckOne.dealStart();
+// deckOne.dealFlop();
+// deckOne.dealTurn();
+// deckOne.dealRiver();
+
+
+// Event Listeners
+dealButton.addEventListener("click", ()=> {
+    deckOne.shuffle();
+    deckOne.dealStart();
+    deckOne.dealFlop();
+})
+// dealButton.addEventListener("click", ()=> {
+//     console.log('deal btn listener 2 connected')
+// })
 
 /**
  * Sticking Points:
  * Had to use for(in) loop instead of a for(of) loop to get the suits to attack to the cards. Reason?
  * Remember to create and push objects not strings
  * Shuffler addes an undefined object to the deck???
+ * Why can't I call the dealFlop method from the Deck class while inside of the event listener?
  */
 /**
  * AHA moments:
  * can tack on an index selector to the end of the splice method in the methods within the Class constructor (removes a step from the process)
  */
-function renderBoard(){
-    
-    
-}
-renderBoard();
+
+//dealButton.addEventListener("click", deckOne.dealFlop)
+
+/**
+ * Link card rank and suit to images
+ * Assign them to 
+ */
+
